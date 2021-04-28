c7c04fa93a562babe62da4d4dd8479090f0cbf37
Avara, Emre <emre_avara@hotmail.com>
2021-04-28 23:49:10 +0300

    Implement download data from server and show on feed activity InstagramCloneParse App

--- InstagramCloneParse/app/src/main/java/com/avara/instagramcloneparse/FeedActivity.java
+++ InstagramCloneParse/app/src/main/java/com/avara/instagramcloneparse/FeedActivity.java
@@ -4,6 +4,8 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -11,15 +13,26 @@ import android.view.MenuItem;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.parse.FindCallback;
+import com.parse.GetDataCallback;
 import com.parse.LogOutCallback;
 import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FeedActivity extends AppCompatActivity {
 
     ListView listView;
+    ArrayList<String> userNamesFromParse;
+    ArrayList<String> userCommentsFromParse;
+    ArrayList<Bitmap> userImagesFromParse;
+
+    PostClass postClass;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -59,5 +72,47 @@ public class FeedActivity extends AppCompatActivity {
         setContentView(R.layout.activity_feed);
 
         listView = findViewById(R.id.listView);
+
+        userNamesFromParse = new ArrayList<>();
+        userCommentsFromParse = new ArrayList<>();
+        userImagesFromParse = new ArrayList<>();
+
+
+        postClass = new PostClass(userNamesFromParse, userCommentsFromParse, userImagesFromParse, this);
+
+        listView.setAdapter(postClass);
+        download();
+
     }
+
+    public void download() {
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Posts");
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> objects, ParseException e) {
+                if(e != null){
+                    Toast.makeText(getApplicationContext(), e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                } else {
+                    if(objects.size() > 0){
+                        for(final ParseObject object : objects){
+                            ParseFile parseFile = (ParseFile) object.get("image");
+                            parseFile.getDataInBackground(new GetDataCallback() {
+                                @Override
+                                public void done(byte[] data, ParseException e) {
+                                    if(e == null && data != null){
+                                        Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+                                        userImagesFromParse.add(bitmap);
+                                        userNamesFromParse.add(object.getString("username"));
+                                        userCommentsFromParse.add(object.getString("comment"));
+                                        postClass.notifyDataSetChanged();
+                                    }
+                                }
+                            });
+                        }
+                    }
+                }
+            }
+        });
+    }
+
 }
